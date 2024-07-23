c2ce1b8d9c9f1297d7243011b5700bd5b5664da2
AmirHossein-Biabani <amirhosseinbiabany@yahoo.com>
2024-07-23 19:59:58 +0330

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Instagram_Common_followers
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            Username = new TextBox();
+            Password = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label3 = new Label();
+            textBox5 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(16, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(60, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Username";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(16, 80);
+            label2.Name = "label2";
+            label2.Size = new Size(57, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Password";
+            // 
+            // Username
+            // 
+            Username.Location = new Point(92, 21);
+            Username.Name = "Username";
+            Username.Size = new Size(264, 23);
+            Username.TabIndex = 2;
+            Username.TextChanged += textBox1_TextChanged;
+            // 
+            // Password
+            // 
+            Password.Location = new Point(92, 77);
+            Password.Name = "Password";
+            Password.Size = new Size(264, 23);
+            Password.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(92, 119);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(264, 23);
+            textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(92, 163);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(264, 23);
+            textBox4.TabIndex = 5;
+            textBox4.TextChanged += textBox4_TextChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(216, 145);
+            label3.Name = "label3";
+            label3.Size = new Size(29, 15);
+            label3.TabIndex = 6;
+            label3.Text = "And";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(12, 242);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(432, 334);
+            textBox5.TabIndex = 7;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(186, 204);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 8;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(460, 588);
+            Controls.Add(button1);
+            Controls.Add(textBox5);
+            Controls.Add(label3);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(Password);
+            Controls.Add(Username);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox Username;
+        private TextBox Password;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label3;
+        private TextBox textBox5;
+        private Button button1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using InstagramApiSharp.API;
+using InstagramApiSharp.API.Builder;
+using InstagramApiSharp.Classes;
+using InstagramApiSharp.Logger;
+
+namespace Instagram_Common_followers
+{
+    public partial class Form1 : Form
+    {
+        private static UserSessionData user;
+        private static IInstaApi api;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+
+            user = new UserSessionData();
+            user.UserName = Username.Text;
+            user.Password = Password.Text;
+            api = InstaApiBuilder.CreateBuilder()
+                .SetUser(user).UseLogger(new DebugLogger(LogLevel.All))
+                .SetRequestDelay(RequestDelay.FromSeconds(0, 1))
+                .Build();
+
+
+            var login = await api.LoginAsync();
+            if (!login.Succeeded)
+                
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
